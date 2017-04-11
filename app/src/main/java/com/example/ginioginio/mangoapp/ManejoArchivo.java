4b219e530f8ae9f4e58fbab0fd2fd541b0f7a88d
gobitmx <gobitmx@gmail.com>
2017-04-10 20:22:57 -0500

    primer commit

---
+++ app/src/main/java/com/example/ginioginio/mangoapp/ManejoArchivo.java
@@ -0,0 +1,25 @@
+package com.example.ginioginio.mangoapp;
+
+import java.io.File;
+import 	java.util.Date;
+
+/**
+ * Created by Bruno on 04/04/2017.
+ */
+
+public class ManejoArchivo {
+
+    public File getArchivo(){
+
+        File directorio = new File("sdcard/mangoapp");
+
+        if(!directorio.exists()){
+            directorio.mkdir();
+        }
+
+        //String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+
+        File imagen = new File(directorio,"app_mango.img");
+        return  imagen;
+    }
+}
